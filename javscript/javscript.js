39c2f046df5ea819ad665f656d8acd0b7958afbe
lxnarayan1 <155988187+lxnarayan1@users.noreply.github.com>
2025-01-01 12:36:50 +0000

    container config done

---
+++ javscript/javscript.js
@@ -0,0 +1,76 @@
+// const item = {
+//   name : "laxmi narayan",
+//   age :  21 ,
+//   phone_number : 888888
+// }
+
+// // let a = "laxmi"
+// // console.log("Type of a is:" ,typeof(a))
+// item.age = 20
+// // console.log(item)
+
+// const marks={
+//   harry : 98,
+//   laxmi : 99,
+//   rohan : 70,
+//   aakash : 7
+// }
+
+// for (let i=0 ; i<Object.keys(marks).length; i++) {
+//   console.log("the marks of"+ Object.keys(marks)[i]  + " are " + marks[Object.keys(marks)[i]])
+// }
+// for (let number in marks){
+//   console.log("the marks of"+ number + " are " + marks[number])
+// }
+
+// let cn=45
+// let i
+
+
+
+// // switch case 
+// let names = 'narry'
+// switch (names){
+//   case lucky:
+//     console.log("this is lucky")
+//     break
+     
+//   case  shlok :
+//     console.log( "this is shlok")
+//     break
+    
+//   case narry:
+//     console.log("this is narry")
+//     break
+//   default: 
+//     console.log("switch default")
+//     break
+//}
+
+
+// let x =prompt("enter the number")
+
+// if (x%2==0 ||x%3==0){
+//   console.log("number is divisible by 2 or 3")}
+// else{
+//   console.log("number is not divisible by 2 or 3")}
+
+// function mean(a ,b ,c){
+//   let mean = (a+b+c)/3
+//   console.log("mean is " + mean)
+//   return mean 
+//   }
+
+// let a = prompt("enter the number")
+// let b = prompt("enter the number")
+// let c = prompt("enter the number")
+// mean(a,b,c)
+
+
+// // console.log("harry".length )
+// let text = "Hello world";
+// let result = text.endsWith("world");
+// console.log(result)
+
+str = "please give rs 1000 "
+console.log(str.replace(str[4],"a"))
